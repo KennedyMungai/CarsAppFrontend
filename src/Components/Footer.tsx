e827111cdd06f8ac309f9286b52578d45b965003
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-01 09:59:17 +0300

    The template for the footer component

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {};
+
+const Footer = (props: Props) => {
+  return (
+    <footer className="flex items-center justify-around w-full">Footer</footer>
+  );
+};
+
+export default Footer;
