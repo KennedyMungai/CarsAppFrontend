d8e30ad62b7ca0f8c1d0fe344b6fbe641cd1e26e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-01 09:57:55 +0300

    Created the prototype for the Header component

---
+++ src/Components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const Header = (props: Props) => {
+  return <nav className="flex items-center justify-around w-full">Header</nav>;
+};
+
+export default Header;
